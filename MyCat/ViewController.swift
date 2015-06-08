26197913e2f184f3bf58dbfd60fc474327f05d18
leeco1 <Lee@Lees-MacBook-Pro.local>
2015-06-08 12:17:37 +0300

    MyCatAge
    Swift app
     In this app, we had a textField for the user to type in their cat’s
    age in human years, then our app multiplied that number by 7 to get the
    Cat’s age in cat years!

--- MyCat/ViewController.swift
+++ MyCat/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //my code
+    
+    @IBOutlet var CatAge: UITextField!
+//
+    
+    @IBOutlet var Lable: UILabel!
+   
+    
+    @IBAction func btmPress(sender: AnyObject) {
+       
+        var Age = CatAge.text.toInt()
+        
+        if Age != nil {
+            Age = Age!*7;
+            Lable.text = "Your cat is \(Age!) years old";
+        }
+        else {
+        Lable.text = "Please enter valid number";
+    
+        }
+        
+    }
+    
+    
+    //no change :
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
